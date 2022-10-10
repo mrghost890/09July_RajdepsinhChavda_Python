883871c0a1649bb19b3aed60fb83d07140b39a3f
Rajdeepsinh Chavda <100362340+mrghost890@users.noreply.github.com>
2022-10-10 13:35:46 +0530

    Delete Q2.py

--- Q2.py
+++
@@ -1,8 +0,0 @@
-# 2.Write a Python program to get the Factorial number of given number.
-
-num = int(input("Enter Your Number:"))
-a = 1
-
-for i in range(1,num+1):
-    a = a*i
-print("The Factorial of",num,"is",a)
