51f37273b320d89346c988496573604473cd1738
Rajdeepsinh Chavda <100362340+mrghost890@users.noreply.github.com>
2022-10-10 13:34:45 +0530

    Delete Q12.py

--- Q12.py
+++
@@ -1,8 +0,0 @@
-#15. Write a Python program to count occurrences of a substring in a string.
-
-message = 'python is popular programming language'
-
-# number of occurrence of 'p'
-print('Number of occurrence of p:', message.count('p'))
-
-# Output: Number of occurrence of p: 4
