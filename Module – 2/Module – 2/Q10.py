1aeb1013c22da1de905826ce3b1f8c4de1884f34
Rajdeepsinh Chavda <100362340+mrghost890@users.noreply.github.com>
2022-10-10 13:33:44 +0530

    Delete Module – 2/Module – 2 directory

--- Module – 2/Module – 2/Q10.py
+++
@@ -1,4 +0,0 @@
-#12. Write a Python program to calculate the length of a string.
-
-str = input("Enter a string: ")
-print("Length of the input string is:", len(str))
--- Module – 2/Module – 2/Q9.py
+++
@@ -1,5 +0,0 @@
-#11. Write a python program to sum of the first n positive integers.
-
-num = int(input("Input a number: "))
-sum_num = (num * (num + 1)) / 2
-print(sum_num)
